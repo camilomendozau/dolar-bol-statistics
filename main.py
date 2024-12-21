cf11c6edd1bad0821898752c8fa16e67bf8b4d48
Camilo Mendoza <camilomen@localhost.localdomain>
2024-12-21 13:58:33 -0400

    Feat: Insercion de loop infinito para automatizar tarea de buscar dato nuevo

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
-import requests
+import requests,time
 from db import Database
 import configparser
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-cantidadEstados = int(config['DEFAULT']['estadossubidos'])
-DB = Database()
-response = requests.get("https://dolarbo.com/exchange-rates.json")
 
-def saveNewElapsedDaysValue():
+
+#response = requests.get("https://dolarbo.com/exchange-rates.json")
+
+def saveNewElapsedDaysValue(dolarEstadosList):
     config['DEFAULT']['estadossubidos'] = str(len(dolarEstadosList))
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
+def fetch_data_from_api():
+    try:
+        response = requests.get('https://dolarbo.com/exchange-rates.json')
+        if response.status_code == 200:
+            #DB = Database()
+            dolarEstadosList = response.json()[0]["history"]
+            if cantidadEstados < len(dolarEstadosList):
+                estadosToUpdate = len(dolarEstadosList) - cantidadEstados
+                for i, estado in enumerate(dolarEstadosList[-int(estadosToUpdate):]):
+                    #DB.insertNewState(estado['timestamp'], estado['buy'], estado['sell'])
+                    print(i, estado)
+                saveNewElapsedDaysValue(dolarEstadosList)
+                #DB.closeConexion()
+        else:
+            print(f"Error en la consulta: {response.status_code}")
+    except Exception as e:
+        print(f"Error al conectar con la API: {e}")
 
-if response.status_code == 200:
-    dolarEstadosList = response.json()[0]["history"]
-    # for dolarday in dolardayslist:
-    #     print(dolarday['timestamp'])
-    #     #print(datetime.strptime(dolarday['timestamp'],"%Y-%m-%dT%H:%M:%SZ"))
+def main():
+    while True:
+        fetch_data_from_api()
+        time.sleep(120)
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    cantidadEstados = int(config['DEFAULT']['estadossubidos'])
+    main()
+# if response.status_code == 200:
+#     dolarEstadosList = response.json()[0]["history"]
+#     # for dolarday in dolardayslist:
+#     #     print(dolarday['timestamp'])
+#     #     #print(datetime.strptime(dolarday['timestamp'],"%Y-%m-%dT%H:%M:%SZ"))
     
-    if cantidadEstados < len(dolarEstadosList):     
-        estadosToUpdate = len(dolarEstadosList) - cantidadEstados
-        for i,estado in enumerate(dolarEstadosList[-int(estadosToUpdate):]):
-            DB.insertNewState(estado['timestamp'],estado['buy'],estado['sell'])
-            print(i,estado)
-        saveNewElapsedDaysValue()
-
-
-
+#     if cantidadEstados < len(dolarEstadosList):     
+#         estadosToUpdate = len(dolarEstadosList) - cantidadEstados
+#         for i,estado in enumerate(dolarEstadosList[-int(estadosToUpdate):]):
+#             #DB.insertNewState(estado['timestamp'],estado['buy'],estado['sell'])
+#             print(i,estado)
+#         saveNewElapsedDaysValue()
